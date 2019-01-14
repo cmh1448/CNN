41216191a7301f1f865bab2089cac25c3afdce28
천명현 <cmh7264@gmail.com>
2019-01-14 15:18:28 +0900

    Create

---
+++ CNN/Library/cuboid.cpp
@@ -0,0 +1,9 @@
+//
+//  cuboid.cpp
+//  CNN
+//
+//  Created by 천명현 on 14/01/2019.
+//  Copyright © 2019 천명현. All rights reserved.
+//
+
+#include "cuboid.hpp"
---
+++ CNN/Library/cuboid.hpp
@@ -0,0 +1,14 @@
+//
+//  cuboid.hpp
+//  CNN
+//
+//  Created by 천명현 on 14/01/2019.
+//  Copyright © 2019 천명현. All rights reserved.
+//
+
+#ifndef cuboid_hpp
+#define cuboid_hpp
+
+#include <stdio.h>
+
+#endif /* cuboid_hpp */
