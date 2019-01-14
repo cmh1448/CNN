ac645423e7745ee081f81ed1ff580c500b4c1ea2
천명현 <cmh7264@gmail.com>
2019-01-14 15:13:23 +0900

    Initial Commit

---
+++ CNN/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CNN
+//
+//  Created by 천명현 on 14/01/2019.
+//  Copyright © 2019 천명현. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
